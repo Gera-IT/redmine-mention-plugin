8e0b610494429267ac5dc03dbf37cc65064f95c8
Alex Sinelnikov <sinelniko@gmail.com>
2015-01-06 17:32:23 +0200

    Added feature to send email to mentioned user

--- lib/mention.rb
+++ lib/mention.rb
@@ -1,9 +1,11 @@
 require_dependency 'issue'
 require_dependency 'watcher'
 require_dependency 'user'
+require_dependency 'issue'
 require_dependency 'wiki_page'
 require 'mention/journal_patch'
 require 'mention/user_patch'
+require 'mention/issue_patch'
 require 'mention/journal_helper_patch'
 require 'mention/wiki_helper_patch'
 require 'mention/wiki_content_patch'
@@ -26,6 +28,7 @@ module Mention
   def self.apply_patch
 	  Journal.send(:include, Mention::JournalPatch)
 	  User.send(:include, Mention::UserPatch)
+		Issue.send(:include, Mention::IssuePatch)
 	  JournalsHelper.send(:include, Mention::JournalHelperPatch)
 	  WikiHelper.send(:include, Mention::WikiHelperPatch)
 	  WikiContent.send(:include, Mention::WikiContentPatch)
---
+++ lib/mention/issue_patch.rb
@@ -0,0 +1,21 @@
+require_dependency 'issue'
+
+module Mention
+  module IssuePatch
+    def self.included(base)
+      base.class_eval do
+        unloadable
+        base.send(:after_save) do |issue|
+          journal = issue.journals.last
+          users = journal.notes.scan(/~[\w.]+/)
+          if journal.try(:notes).try(:present?) && users.count > 0
+            users = users.map {|u| u[1..-1]} #to remove ~ before user_name
+            users = User.where(login: users)
+            users = users.reject {|u| u.mail_notification == "none"}
+            Mailer.issue_edit(issue.journals.last, users, users).deliver
+          end
+        end
+      end
+    end
+  end
+end
