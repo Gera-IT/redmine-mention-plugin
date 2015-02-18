8051892e5d9ade32661bb735529e33718c780422
Alex Sinelnikov <sinelniko@gmail.com>
2015-02-18 14:11:41 +0200

    added missing images

--- assets/javascripts/redmine-mention.js
+++ assets/javascripts/redmine-mention.js
@@ -1,14 +1,11 @@
 $(function(){
     $('.redactor-editor').mentionsInput({source: function(request, response){
-        console.log(request);
         project_id = $('form').first().attr('action').replace( /\/projects\//, '' );
         project_id = project_id.replace( /\/search/, '' );
-        var data = [];
         $.ajax({
             url: '/mention/search',
             data: {'search_tag': request, project_id: project_id},
             success: function(result) {
-                console.log(result);
                 response(result['users'])
             }
         });
@@ -18,15 +15,12 @@ $(function(){
 setTimeout(function(){
     $(document).ready(function(){
         $('.redactor-editor').mentionsInput({source: function(request, response){
-            console.log(request);
             project_id = $('form').first().attr('action').replace( /\/projects\//, '' );
             project_id = project_id.replace( /\/search/, '' );
-            var data = [];
             $.ajax({
                 url: '/mention/search',
                 data: {'search_tag': request, project_id: project_id},
                 success: function(result) {
-                    console.log(result);
                     response(result['users'])
                 }
             });
